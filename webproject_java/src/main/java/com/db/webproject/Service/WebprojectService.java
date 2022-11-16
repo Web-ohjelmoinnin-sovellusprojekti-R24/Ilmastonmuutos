393c26eae3c16dda5e6b17d50ee63b691ea91b36
OnniNurkkala <101476135+OnniNurkkala@users.noreply.github.com>
2022-11-16 14:22:17 +0200

    Add files via upload

---
+++ webproject_java/src/main/java/com/db/webproject/Service/WebprojectService.java
@@ -0,0 +1,23 @@
+package com.db.webproject.Service;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.db.webproject.Data.V1;
+import com.db.webproject.Data.V1Repository;
+
+
+@Service
+public class WebprojectService {
+    @Autowired
+    V1Repository v1Repository;
+
+    public List<V1> getAllData(){
+        return v1Repository.findAll();    
+
+    }
+}
