eb4e5c4725d5250011f72e6f9f0b373129dacef9
OnniNurkkala <101476135+OnniNurkkala@users.noreply.github.com>
2022-11-16 14:13:29 +0200

    Add files via upload

---
+++ WebProjectG24/webproject_react/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import V1 from './components/V1';
+
+function App() {
+
+  return (
+    <div>
+      <div>
+        <V1/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
