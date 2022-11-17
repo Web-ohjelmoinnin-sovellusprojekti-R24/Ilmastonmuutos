3bdd6fb60b1fed746dc7f09a8b4a96eb02a11054
SKIPAH <juzzii.mehta@gmail.com>
2022-11-17 23:27:47 +0200

    github test + sivuja

--- webproject_react/src/App.js
+++ webproject_react/src/App.js
@@ -1,14 +1,26 @@
 import './App.css';
+import Login from './components/Login';
+import Home from './components/Home';
 import V1 from './components/V1';
+import CreateView from './components/CreateView'
+import {Routes, Route, BrowserRouter, Link} from "react-router-dom";
 
 function App() {
 
   return (
     <div>
-      <div>
-        <V1/>
+      <div className='navbar'>
+        <div>Create new view</div>
+        <div>Logout</div>
+        <div>Temperature data and co2 concentrations</div>
+        <div>Emission sources</div>
       </div>
-    </div>
+        <Routes>
+        <Route path="/" element={<Login/>}/>
+        <Route path='/views' element={<CreateView/>}/>
+        <Route path="/v1" element={<V1/>}/>
+        </Routes>
+    </div> 
   );
 }
 
---
+++ webproject_react/src/components/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+
+export default function Login() {
+return (
+    <div>
+        <h1>History Of Climate Changes</h1>
+        <Link to="/v1"><button>Login</button></Link>
+    </div>
+)
+}
