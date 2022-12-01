7a595b902caf9d72656672da6aba6e6a12ac12d0
SKIPAH <juzzii.mehta@gmail.com>
2022-12-01 15:41:59 +0200

    läppäricommit + kokodatabase

--- webproject_react/src/App.js
+++ webproject_react/src/App.js
@@ -4,7 +4,6 @@ import Home from './components/Home';
 import CreateView from './components/CreateView'
 import {Routes, Route, BrowserRouter, Link} from "react-router-dom";
 import V5 from './components/V5';
-import V4 from './components/V4';
 import V9_try from './components/v9_try';
 import V1_V2 from './components/V1_V2';
 import V3 from './components/V3';
@@ -26,12 +25,11 @@ function App() {
         <Route path="/v1" element={<V1_V2/>}/>
         <Route path="/v3" element={<V3/>}/>
         <Route path="/v5" element={<V5/>}/>
-        <Route path="/v4" element={<V4/>}/>
         <Route path="/v9" element={<V9_try/>}/>
         </Routes>
     </div> 
   );
 }
-//    <V1_V2/>
+
 
 export default App;
